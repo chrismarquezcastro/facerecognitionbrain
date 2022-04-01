8f9dca45c7fb5ffe9b86f87854a045c1f5c8b828
Chris Marquez <ixdchrismarquez@gmail.com>
2022-04-01 16:03:18 -0700

    Draw line around the face coordinates.

--- src/components/FaceRecognition/FaceRacognition.js
+++ src/components/FaceRecognition/FaceRacognition.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ imageURL, box }) => {
   return (
-    <div className="center ma mt4">
-      <img alt="Analized result" src={imageURL} />
+    <div className="center ma">
+      <div className="mt4 absolute center">
+        <img id="inputImage" alt="Analized result" src={imageURL} />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
